1dd7a41082305e7e70c6ab7069010b85c446e910
liuzhiqiang <woshixinfode@sina.com>
2018-09-06 14:53:12 +0800

    all

---
+++ src/components/testphone.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="phonebody">
+        手机移动端
+        <div class="banner">
+            shangke 
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'testphone',
+  data () {
+    return {
+      msg: 'testphone'
+    }
+  }
+}
+</script>
+<style >
+    .phonebody{
+        width:650px;
+        padding:50px;
+    }
+    .banner{width:200px;font-size:28px;}
+</style>
